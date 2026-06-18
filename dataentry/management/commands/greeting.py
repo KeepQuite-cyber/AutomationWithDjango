54f0484da7e80ce6616ec9d609717d709ac5ea73
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-18 11:05:49 +0530

    AWD-04

--- dataentry/management/commands/greeting.py
+++ dataentry/management/commands/greeting.py
@@ -2,9 +2,17 @@ from django.core.management.base import BaseCommand
 
 
 # Proposed Command -> python manage.py greeting
+# Proposed output -> Hi {name} , Good Morning 
 class Command(BaseCommand):
-    help = "Greeting"
+    help = "Greeting the user"
+
+
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str , help='Specifies user name')
+    
 
     def handle(self , *args , **kwargs):
         # Write the logic
-        self.stdout.write("Greetings from Priyanshu Tripathi.")
+        name = kwargs['name']
+        greeting = f'Hii {name} , Good Morning' 
+        self.stdout.write(self.style.SUCCESS(greeting))
