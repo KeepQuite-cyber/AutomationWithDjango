23352c52a1d83acad7677d46f11772e6f712088b
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-19 08:07:55 +0530

    AWD-08

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -16,7 +16,15 @@ class Command(BaseCommand):
             {'name' : "Raghav" , 'roll_no' : 17 , 'age' : 18},
             {'name' : "Muskan" , 'roll_no' : 67 , 'age' : 21},
             {'name' : "Bro" , 'roll_no' : 11 , 'age' : 22},
+            {'name' : "Shiljd" , 'roll_no' : 1005 , 'age' : 22},
         ]
+
         for data in dataset:
-            Student.objects.create(name=data['name'] , roll_no=data['roll_no'] , age=data['age'])
-        self.stdout.write(self.style.SUCCESS("Data inserted successfully !"))
+            roll_no = data['roll_no']
+            existing_record = Student.objects.filter(roll_no=roll_no).exists()
+            if not existing_record:
+                Student.objects.create(name=data['name'] , roll_no=data['roll_no'] , age=data['age'])
+
+            else:
+                self.stdout.write(self.style.WARNING(f"Student with roll no {roll_no} already exists !"))
+        self.stdout.write(self.style.SUCCESS('Data Inserted successffully !'))
